e3504dc7b42ccc0da2bef4c91b924002c08c7fb9
Darkleo <cestleonard@aol.com>
2010-07-03 12:04:46 +0200

    alternative launch for tests

---
+++ test/uby2d.rb
@@ -0,0 +1,46 @@
+# Ruby 2D library
+# Alternative require
+# by Darkleo
+
+p 'alternative launch'
+
+# Require libraries
+require 'opengl'
+require 'glut'
+require 'zlib'
+require 'Win32API'
+
+module Ruby2D
+  extend self
+
+  MessageBox = Win32API.new('user32','MessageBox','lppl','i')
+  # Create a small windows where args will be showed
+  #
+  # call .to_s when necessary
+  def popup *args
+    puts *args
+    MessageBox.call(0, args*"\n", 'Popup :', 0)
+    nil
+  end
+end
+include Ruby2D
+
+Dir.chdir File.dirname(__FILE__)
+#~ $: << File.dirname(__FILE__)
+#~ p $:[-1]
+# Low level
+require '../lib/ruby2D/application'
+require '../lib/ruby2D/window'
+require '../lib/ruby2D/mouse'
+require '../lib/ruby2D/input'
+# High level
+require '../lib/ruby2D/cache'
+require '../lib/ruby2D/texture'
+require '../lib/ruby2D/sprite'
+# Modules
+require '../lib/ruby2D/graphics'
+# TODO : audio
+# require 'ruby2D/audio' # Soon !
+# Stuff
+require '../lib/ruby2D/shapes'
+require '../lib/ruby2D/color'
