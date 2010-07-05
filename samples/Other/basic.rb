25172c6333be4e8459eee9d400568a8f81965868
Darkleo <cestleonard@aol.com>
2010-07-05 19:19:37 +0200

    added samples

---
+++ samples/Other/basic.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'uby2d'
+
+Cache.add_location 'Other/'
+app = Application.new :name => 'light', :width => 128, :height => 128
+app.launch {
+  @sprite = Sprite.new :texture => Texture.new('Star.bmp')
+  @sprite.ox = @sprite.oy =  @sprite.x = @sprite.y = 64
+  loop {
+    @sprite.angle += 1
+    sleep 0.02
+  }
+}
