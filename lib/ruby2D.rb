738574a9f333e5d84f3cd7d0bc24df370cdeed0d
Darkleo <cestleonard@aol.com>
2010-11-03 00:01:40 +0100

    Global update

--- lib/ruby2D.rb
+++ lib/ruby2D.rb
@@ -1,42 +1,37 @@
 # Ruby 2D library
 # by Darkleo
 
-# Require libraries
+# Extern libraries
 require 'opengl'
 require 'glut'
 require 'fmod'
 require 'zlib'
 require 'Win32API'
 
+# Stuff ...
 Thread.abort_on_exception = true
 $>.sync = true
 
+# Ruby2D
 module Ruby2D
-  extend self
-  
-  # Constants
-  MessageBox = Win32API.new 'user32','MessageBox','lppl','i'
-  
   # Popup
-  def popup *args
+  MessageBox = Win32API.new 'user32','MessageBox','lppl','i'
+  def self.popup *args
     puts *args
     MessageBox.call 0, args*"\n", 'Popup :', 0
     nil
   end
+  Mutex = Mutex.new
 end
-include Ruby2D
 
-# High level
+# Ruby2D library
 require 'ruby2D/graphic'
 require 'ruby2D/sprite'
 require 'ruby2D/cache'
 require 'ruby2D/bitmap'
-# Low level
 require 'ruby2D/window'
 require 'ruby2D/mouse'
 require 'ruby2D/input'
-# Modules
 require 'ruby2D/graphics'
-# Stuff
 require 'ruby2D/shapes'
 require 'ruby2D/color'
