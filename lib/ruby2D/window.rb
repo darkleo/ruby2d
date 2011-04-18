c2381c11d9606c855f8c2f168aa92b51c476303f
darkleo <cestleonard@aol.com>
2011-04-18 10:47:17 +0200

    Window.screenshot

--- lib/ruby2D/window.rb
+++ lib/ruby2D/window.rb
@@ -44,6 +44,12 @@ module Ruby2D
   def fullscreen?
     @fullscreen
   end
+  def screenshot x, y, w, h
+    temp = []
+    @to_set[:screenshot] = [temp, x, @height-h-y, w, h]
+    sleep(0) while temp.empty?
+    temp.first
+  end
   
   def run &block
     GLUT.Init
@@ -89,116 +95,118 @@ module Ruby2D
     GL.Disable GL::CULL_FACE
     
     Thread.new { yield block }
-    GLUT.MainLoop()
+    GLUT.MainLoop
   end
   
   private
   def create_procs
-  @procs = {}
-  @procs[:reshape] = proc { |w, h|
-    width  = [1, w].max
-    height = [1, h].max
-    GL.Viewport(0, 0,  width,  height)
-    GL.MatrixMode(GL::PROJECTION)
-    GL.LoadIdentity()
-    GL.Ortho(0.0, width, -height, 0.0, -1, 1)
-    GL.MatrixMode(GL::MODELVIEW)
-    @width  = width
-    @height = height
-  }
-  @procs[:display] = lambda {
-    GL.Clear GL::COLOR_BUFFER_BIT
-    Graphics.output
-    GLUT.SwapBuffers()
-    GLUT.PostRedisplay()
-  }
-  #@procs[:timer] = lambda {|i|
-    #GLUT.TimerFunc(10, @procs[:timer], i)
-  #}
-  @procs[:keyboard] = lambda {|key,x,y|
-    exit if key == ?\e
-    Input.feed key => :trig
-    gm = GLUT.GetModifiers
-    case gm
-    when 1 ; Input.feed 'maj' => :press
-    when 2 ; Input.feed 'ctrl' => :press
-    when 4 ; Input.feed 'alt' => :press
-    when 6 ; Input.feed 'altg' => :press
+    @procs = {}
+    @procs[:reshape] = lambda do |w, h|
+      width  = [1, w].max
+      height = [1, h].max
+      GL.Viewport(0, 0,  width,  height)
+      GL.MatrixMode(GL::PROJECTION)
+      GL.LoadIdentity()
+      GL.Ortho(0.0, width, -height, 0.0, -1, 1)
+      GL.MatrixMode(GL::MODELVIEW)
+      @width  = width
+      @height = height
     end
-  }
-  @procs[:keyboard_up] = lambda {|key,x,y|
-    Input.feed key.chr => :release
-    gm = GLUT.GetModifiers
-    case gm
-    when 1 ; Input.feed 'maj' => :press
-    when 2 ; Input.feed 'ctrl' => :press
-    when 4 ; Input.feed 'alt' => :press
-    when 6 ; Input.feed 'altg' => :press
+    @procs[:display] = lambda do
+      GL.Clear GL::COLOR_BUFFER_BIT
+      Graphics.output
+      GLUT.SwapBuffers()
     end
-  }
-  @procs[:special_keyboard] = lambda {|key,x,y|
-    Input.feed get_special_key(key) => :trig
-  }
-  @procs[:special_keyboard_up] = lambda {|key,x,y|
-    Input.feed get_special_key(key) => :release
-  }
-  @procs[:mouse] = lambda {|key,state,x,y|
-    case key
-    when 0 ; nkey = Mouse::Left
-    when 1 ; nkey = Mouse::Middle
-    when 2 ; nkey = Mouse::Right
-    when 3 ; nkey = Mouse::Wheel_Up
-    when 4 ; nkey = Mouse::Wheel_Down
+    #~ @procs[:timer] = lambda do |i|
+      #~ GLUT.TimerFunc(10, @procs[:timer], i)
+    #~ end
+    @procs[:keyboard] = lambda do |key,x,y|
+      exit if key == ?\e
+      Input.feed key => :trig
+      gm = GLUT.GetModifiers
+      case gm
+      when 1 ; Input.feed 'maj' => :press
+      when 2 ; Input.feed 'ctrl' => :press
+      when 4 ; Input.feed 'alt' => :press
+      when 6 ; Input.feed 'altg' => :press
+      end
     end
-    case state
-    when 0 ; nstate = :down
-    when 1 ; nstate = :up
+    @procs[:keyboard_up] = lambda do |key,x,y|
+      Input.feed key.chr => :release
+      gm = GLUT.GetModifiers
+      case gm
+      when 1 ; Input.feed 'maj' => :press
+      when 2 ; Input.feed 'ctrl' => :press
+      when 4 ; Input.feed 'alt' => :press
+      when 6 ; Input.feed 'altg' => :press
+      end
     end
-    Mouse.feed nkey => nstate, :x => x, :y => y
-  }
-  @procs[:mouse_passive] = lambda {|x,y|
-    Mouse.feed :x => x, :y => y
-  }
-  @procs[:motion] = lambda {|x,y|
-    Mouse.feed :x => x, :y => y
-  }
-  @procs[:idle] = lambda {
-    #~ GLUT.PostRedisplay()
-    return unless @to_set
-		@to_set.each_pair do |key, value|
+    @procs[:special_keyboard] = lambda do |key,x,y|
+      Input.feed get_special_key(key) => :trig
+    end
+    @procs[:special_keyboard_up] = lambda do |key,x,y|
+      Input.feed get_special_key(key) => :release
+    end
+    @procs[:mouse] = lambda do |key,state,x,y|
       case key
-      when :name
-        GLUT.SetWindowTitle value+@name_suffix
-      when :name_suffix
-        GLUT.SetWindowTitle @name+value 
-      when :size
-        GLUT.ReshapeWindow(*value)
-      when :position
-        GLUT.PositionWindow(*value)
-      when :fullscreen
-        if value
-          GLUT.FullScreen
-        else
-          resize @width, @height
+      when 0 ; nkey = Mouse::Left
+      when 1 ; nkey = Mouse::Middle
+      when 2 ; nkey = Mouse::Right
+      when 3 ; nkey = Mouse::Wheel_Up
+      when 4 ; nkey = Mouse::Wheel_Down
+      end
+      case state
+      when 0 ; nstate = :down
+      when 1 ; nstate = :up
+      end
+      Mouse.feed nkey => nstate, :x => x, :y => y
+    end
+    @procs[:mouse_passive] = lambda do |x,y|
+      Mouse.feed :x => x, :y => y
+    end
+    @procs[:motion] = lambda do |x,y|
+      Mouse.feed :x => x, :y => y
+    end
+    @procs[:idle] = lambda do
+      return unless @to_set
+      @to_set.each_pair do |key, value|
+        case key
+        when :name
+          GLUT.SetWindowTitle value+@name_suffix
+        when :name_suffix
+          GLUT.SetWindowTitle @name+value 
+        when :size
+          GLUT.ReshapeWindow(*value)
+        when :position
+          GLUT.PositionWindow(*value)
+        when :fullscreen
+          if value
+            GLUT.FullScreen
+          else
+            resize @width, @height
+          end
+        when :screenshot
+          # ??? GLUT.PostRedisplay
+          # ??? GLUT.SwapBuffers
+          value[0] << GL.ReadPixels(value[1], value[2], value[3], value[4], GL::RGBA, GL::UNSIGNED_BYTE)
         end
       end
-		end
-		@to_set.clear
-  }
-  #@procs[:entry] = lambda {|state|
-    #@state = state == 1 # 0:left / 1:entered
-  #}
-  #~ @procs[:visibility] = lambda {|v|
-  #~ }
-  #~ @procs[:window_status] = lambda {|state|
-  #~ }
+      @to_set.clear
+    end
+    #~ @procs[:entry] = lambda do |state|
+      #~ @state = state == 1 # 0:left / 1:entered
+    #~ end
+    #~ @procs[:visibility] = lambda do |v|
+    #~ end
+    #~ @procs[:window_status] = lambda do |state|
+    #~ end
   end
   
   SpecialKeys = %w/F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12
     DOWN LEFT RIGHT UP PAGE_UP PAGE_DOWN
     END HOME INSERT/
   def get_special_key n
-    SpecialKeys.each {|k| return k if eval("GLUT::KEY_#{k} == n")}
+    SpecialKeys.each {|k| return k if GLUT.const_get('KEY_'+k) == n}
   end
 end.new
 end
