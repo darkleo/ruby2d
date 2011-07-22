c0f7daf3b4d064149c04a0627724609bb3c5c8c7
darkleo <cestleonard@aol.com>
2011-07-22 23:56:41 +0200

    real empty sample

--- samples/empty.rb
+++ samples/empty.rb
@@ -1,9 +1,2 @@
 require_relative '../lib/ruby2D'
-include Ruby2D
-
-Cache.add_location 'Other/'
-Window.name = 'Empty'
-Window.resize 256, 256
-Window.run do
-  loop { Graphics.update }
-end
+Ruby2D::Window.run {}
