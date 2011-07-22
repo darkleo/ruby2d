756d16343fade1cd4236b27a8a0172aa67c93c37
darkleo <cestleonard@aol.com>
2011-07-23 00:06:11 +0200

    metaprogramming and new main loop system

--- lib/ruby2D/graphics.rb
+++ lib/ruby2D/graphics.rb
@@ -1,53 +1,44 @@
-module Ruby2D
-module Graphics
-  extend self
-  
-  @main_frame = Frame.new nil
-  attr_accessor :main_frame
+class Ruby2D::Graphics
+  @framecount = 0
+  @fps_last_time = 0
+  @fps_last_count = 0
+  @fps_in_title = false
+  @need_bind = true
+  @main_frame = Ruby2D::Frame.new nil
   def @main_frame.dispose
     fail 'You cannot simply dispose the main Frame'
   end
-  
-  @frame_count = 0
-  @fps = 0
-  @fps_last_time = 0
-  @fps_in_title = false
-  @need_bind = true
-  
+end
+
+class << Ruby2D::Graphics       
+  attr_accessor :main_frame
   attr_reader :framerate
-  attr_accessor :frame_count
+  attr_accessor :framecount
   attr_accessor :fps_in_title
   attr_accessor :need_bind, :need_update
   
   def frame_reset
-    @last_time = GLUT.Get GLUT::ELAPSED_TIME
+    @last_time = @framecount == 0 ? 0 : GLUT.Get(GLUT::ELAPSED_TIME)
   end
   def framerate= fps
     @framerate = fps
-    @waiting_time = 1000/fps*0.8
-    @fps_delay = fps == 1.0/0 ? 500 : 2*fps/3
-    if @frame_count == 0
-      # Glut isn't launched yet
-      @last_time = 0
-    else
-      frame_reset
-    end
+    @waiting_time = (1000/fps).to_i
+    frame_reset
   end
-  self.framerate = 60
 
   def update force=false
-    now = GLUT.Get GLUT::ELAPSED_TIME
-    while now - @last_time < @waiting_time
-      now = GLUT.Get GLUT::ELAPSED_TIME
-    end
-    @frame_count += 1
+    now = GLUT.Get(GLUT::ELAPSED_TIME)
+    delta = @last_time + @waiting_time - now
+    sleep (delta-1)/1000.0 if delta > 0
+    
+    @framecount += 1
     @last_time = now
     GLUT.PostRedisplay()
     
     return unless @fps_in_title
-    if @frame_count % @fps_delay == 0
-      @fps = @fps_delay*1000/(now-@fps_last_time) rescue 'Infinity'
-      Window.name_suffix = " - #@fps fps"
+    if now - @fps_last_time > 1000
+      Window.name_suffix = " - #{(@framecount - @fps_last_count)*1000/(now-@fps_last_time)} fps"
+      @fps_last_count = @framecount
       @fps_last_time = now
     end
   end
@@ -55,4 +46,4 @@ module Graphics
     @main_frame.output
   end
 end
-end
+Ruby2D::Graphics.framerate = 60
